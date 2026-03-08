d354b353dffe2d19a5dae5beadb30117fe7298c4
kevin-trochon <157088235+Techktr@users.noreply.github.com>
2026-03-08 19:43:28 +1100

    feat: implémentation du détail du praticien.

--- src/app/practitioner/practitioner-details/practitioner-details.ts
+++ src/app/practitioner/practitioner-details/practitioner-details.ts
@@ -1,11 +1,34 @@
-import { Component } from '@angular/core';
+import {Component, inject, OnInit} from '@angular/core';
+import {ActivatedRoute, Router, RouterLink} from '@angular/router';
+import {PractitionerService} from '../practitioner';
+import {Practitioner} from '../models/practitioner.interface';
 
 @Component({
   selector: 'app-practitioner-details',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './practitioner-details.html',
   styleUrl: './practitioner-details.css',
 })
-export class PractitionerDetails {
+export class PractitionerDetails implements OnInit {
+
+  private route: ActivatedRoute = inject(ActivatedRoute);
+  private id: string| null = null;
+  private router:Router = inject(Router);
+  private practitionerService: PractitionerService = inject(PractitionerService);
+  public practitioner: Practitioner | undefined;
+
+  ngOnInit(): void {
+    this.id = this.route.snapshot.params['id'];
+    if (this.id !== null) {
+      this.practitionerService.getPractitioner(parseInt(this.id)).subscribe(practitioner => {this.practitioner = practitioner;});
+    }
+  }
+
+  public onDelete(id: number | undefined): void {
+    if (typeof id === 'undefined') {
+      throw Error('Not implemented');
+    }
+    this.practitionerService.deletePractitioner(id).subscribe(() => this.router.navigate(['practitioners']) );
+  }
 
 }
