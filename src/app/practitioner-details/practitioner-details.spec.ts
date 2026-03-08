31753ab44600f6b9c1cd2658f3dcbaaa4691809d
kevin-trochon <157088235+Techktr@users.noreply.github.com>
2026-03-08 19:08:33 +1100

    feat: ajout du composant practitioner-details

---
+++ src/app/practitioner-details/practitioner-details.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { PractitionerDetails } from './practitioner-details';
+
+describe('PractitionerDetails', () => {
+  let component: PractitionerDetails;
+  let fixture: ComponentFixture<PractitionerDetails>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [PractitionerDetails]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(PractitionerDetails);
+    component = fixture.componentInstance;
+    await fixture.whenStable();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/practitioner-details/practitioner-details.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-practitioner-details',
+  imports: [],
+  templateUrl: './practitioner-details.html',
+  styleUrl: './practitioner-details.css',
+})
+export class PractitionerDetails {
+
+}
