81e6a7c6ef52b35b38b15a5c547bffbd9545b3b3
kevin-trochon <157088235+Techktr@users.noreply.github.com>
2026-03-08 19:29:13 +1100

    feat: implémentation de practitioner-list

--- src/app/practitioner/practitioner-list/practitioner-list.ts
+++ src/app/practitioner/practitioner-list/practitioner-list.ts
@@ -1,11 +1,25 @@
-import { Component } from '@angular/core';
+import {Component, inject, OnInit} from '@angular/core';
+import {PractitionerService} from '../practitioner';
+import {Practitioner} from '../models/practitioner.interface';
+import {RouterLink} from '@angular/router';
 
 @Component({
   selector: 'app-practitioner-list',
-  imports: [],
+  imports: [
+    RouterLink
+  ],
   templateUrl: './practitioner-list.html',
   styleUrl: './practitioner-list.css',
 })
-export class PractitionerList {
+export class PractitionerList implements OnInit {
+  private practitionerService: PractitionerService = inject(PractitionerService);
+  public practitioners: Practitioner[] = [];
+
+  ngOnInit(): void {
+    this.practitionerService.getPractitioners().subscribe(practitioners => {
+      this.practitioners = practitioners;
+    });
+  }
+
 
 }
