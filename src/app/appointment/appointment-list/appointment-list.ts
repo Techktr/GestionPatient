aa0447afeb5bf6b7971958ae344085b199a42de8
kevin-trochon <157088235+Techktr@users.noreply.github.com>
2026-03-29 18:02:55 +1100

    feat: implémentation du composant appointment-list.

--- src/app/appointment/appointment-list/appointment-list.ts
+++ src/app/appointment/appointment-list/appointment-list.ts
@@ -1,11 +1,24 @@
-import { Component } from '@angular/core';
+import {ChangeDetectorRef, Component, inject, OnInit } from '@angular/core';
+import {AppointmentService} from '../appointment';
+import {Appointment} from '../models/appointment.interface';
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-appointment-list',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './appointment-list.html',
   styleUrl: './appointment-list.css',
 })
-export class AppointmentList {
+export class AppointmentList implements OnInit {
+    private appointmentService: AppointmentService = inject(AppointmentService);
+    public appointments: Appointment[] = [];
+    private cdr: ChangeDetectorRef = inject(ChangeDetectorRef);
+
+    ngOnInit(): void {
+        this.appointmentService.getAppointments().subscribe(appointments => {
+          this.appointments = appointments;
+          this.cdr.detectChanges();
+        });
+    }
 
 }
