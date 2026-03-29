e3c5bf513b7a14b701aa931bbfb6faf54ce56e35
kevin-trochon <157088235+Techktr@users.noreply.github.com>
2026-03-29 17:42:06 +1100

    feat: ajouts des routes pour les rendez-vous.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,6 +6,9 @@ import {PatientForm} from './patient/patient-form/patient-form';
 import {PractitionerList} from './practitioner/practitioner-list/practitioner-list';
 import {PractitionerForm} from './practitioner/practitioner-form/practitioner-form';
 import {PractitionerDetails} from './practitioner/practitioner-details/practitioner-details';
+import {AppointmentList} from './appointment/appointment-list/appointment-list';
+import {AppointmentForm} from './appointment/appointment-form/appointment-form';
+import {AppointmentDetails} from './appointment/appointment-details/appointment-details';
 
 export const routes: Routes = [
   {path:"", component: Home},
@@ -17,4 +20,8 @@ export const routes: Routes = [
   {path: "practitioners/create", component: PractitionerForm},
   {path: "practitioners/:id/edit", component: PractitionerForm},
   {path: "practitioners/:id", component: PractitionerDetails},
+  {path: "appointments", component: AppointmentList},
+  {path: "appointments/create", component: AppointmentForm},
+  {path: "appointments/:id/edit", component: AppointmentForm},
+  {path: "appointments/:id", component: AppointmentDetails}
 ];
